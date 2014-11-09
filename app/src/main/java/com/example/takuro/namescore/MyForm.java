664fac74e26bd8e552524d06efc4868aca5bb253
Honda Takuro <gism3ts@yahoo.co.jp>
2014-11-09 14:48:25 +0900

    first commit

---
+++ app/src/main/java/com/example/takuro/namescore/MyForm.java
@@ -0,0 +1,45 @@
+package com.example.takuro.namescore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyForm extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_form);
+    }
+
+    public void getScore(View v){
+        EditText myEditText = (EditText) findViewById(R.id.myEditText);
+        String myName =
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
